633a8c62e54596fd62fd2ba277fab28303199501
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-08-26 14:24:11 +0530

    Project Completed | 80%

--- src/Components/Podcast/AudioPlayer.jsx
+++ src/Components/Podcast/AudioPlayer.jsx
@@ -2,22 +2,95 @@ import React, { useEffect, useRef, useState } from "react";
 import { FaPlay, FaPause, FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 
 const AudioPlayer = ({ audioSrc, image }) => {
-	const [duration, setDuration] = useState("");
-	const [volume, setVolume] = useState(1);
+	const [isPlaying, setIsPlaying] = useState(true);
 	const [isMute, setIsMute] = useState(false);
 	const [isVolumeBtnClicked, setIsVolumeBtnClicked] = useState(false);
-	const [isPlaying, setIsPlaying] = useState(true);
+	const [currentTime, setCurrentTime] = useState(0);
+	const [duration, setDuration] = useState(0);
+	const [volume, setVolume] = useState(0.1);
+
+	// Ref:
 	const audioRef = useRef();
 
-	const handleDuration = (e) => {
-		e.preventDefault();
+	// Custom Functions:
+	const handleTimeUpdate = () => {
+		const currentTime = audioRef.current.currentTime;
+		setCurrentTime(currentTime);
 	};
+
+	const handleLoadedMetadata = () => {
+		const duration = audioRef.current.duration;
+		setDuration(duration);
+	};
+
+	const handleDuration = (e) => {
+		const newTime = parseFloat(e.target.value);
+		setCurrentTime(newTime);
+		audioRef.current.currentTime = newTime;
+	};
+
+	const handleEnded = () => {
+		setCurrentTime(0);
+		setIsPlaying(false);
+	};
+
+	const handleVolume = (e) => {
+		const newVolume = e.target.value;
+		setVolume(newVolume);
+		audioRef.current.volume = newVolume;
+	};
+
+	const togglePlay = () => {
+		if (isPlaying) {
+			setIsPlaying(false);
+		} else {
+			setIsPlaying(true);
+		}
+	};
+
+	const toggleMute = () => {
+		if (isMute) {
+			setIsMute(false);
+		} else {
+			setIsMute(true);
+		}
+	};
+
+	const formatTime = (time) => {
+		const minutes = Math.floor(time / 60);
+		const seconds = Math.floor(time % 60);
+		return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+	};
+
+	// Use Effects:
+	useEffect(() => {
+		const audio = audioRef.current;
+		audio.addEventListener("timeupdate", handleTimeUpdate);
+		audio.addEventListener("loadedmetadata", handleLoadedMetadata);
+		audio.addEventListener("ended", handleEnded);
+
+		return () => {
+			audio.removeEventListener("timeupdate", handleTimeUpdate);
+			audio.removeEventListener("loadedmetadata", handleLoadedMetadata);
+			audio.removeEventListener("ended", handleEnded);
+		};
+	}, []);
 
 	useEffect(() => {
 		if (isPlaying) {
-			audioRef.play();
+			audioRef.current.play();
+		} else {
+			audioRef.current.pause();
 		}
 	}, [isPlaying]);
+
+	useEffect(() => {
+		if (!isMute) {
+			audioRef.current.volume = volume;
+		} else {
+			audioRef.current.volume = 0;
+		}
+	}, [isMute]);
 
 	return (
 		<section className='bg-pg mt-6 relative'>
@@ -32,22 +105,25 @@ const AudioPlayer = ({ audioSrc, image }) => {
 				<div className='flex items-center gap-3 w-[80%]'>
 					<audio ref={audioRef} src={audioSrc} />
 					{/* Buttons */}
-					{isPlaying ? (
-						<FaPlay onClick={() => setIsPlaying((prev) => !prev)} />
-					) : (
-						<FaPause onClick={() => setIsPlaying((prev) => !prev)} />
-					)}
+					<div onClick={togglePlay}>
+						{isPlaying ? <FaPlay /> : <FaPause />}{" "}
+					</div>
 					{/* Time Line */}
-					<div className='flex gap-3 items-center w-full'>
-						<p className='select-none'>0:00</p>
+					<div className='flex gap-1 items-center w-full'>
+						<p className='select-none flex-none'>{formatTime(currentTime)}</p>
 
 						<input
 							type='range'
+							max={duration}
+							value={currentTime}
 							onChange={handleDuration}
-							className='hue-rotate-[25deg] w-[80%] '
+							step={0.01}
+							className='hue-rotate-[25deg] w-[80%] flex-none'
 						/>
 
-						<p className='select-none'>-20:00</p>
+						<p className='select-none flex-none'>
+							-{formatTime(duration - currentTime)}
+						</p>
 					</div>
 				</div>
 
@@ -60,12 +136,22 @@ const AudioPlayer = ({ audioSrc, image }) => {
 							/>
 							{isVolumeBtnClicked && (
 								<div className='bg-black p-3 -rotate-90 absolute bottom-[106px] -right-16 rounded-lg'>
-									<input type='range' />
+									<input
+										type='range'
+										value={volume}
+										onChange={handleVolume}
+										min={0}
+										max={1}
+										step={0.01}
+									/>
 								</div>
 							)}
 						</span>
 
-						<FaVolumeMute onClick={() => setIsMute((prev) => !prev)} />
+						<FaVolumeMute
+							className={isMute ? "text-red-500" : ""}
+							onClick={toggleMute}
+						/>
 					</p>
 				</div>
 			</div>
